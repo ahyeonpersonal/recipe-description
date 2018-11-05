1cd712d529ec47e3f05594dd67d054a4524cd880
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-06 11:52:51 +1300

    Displaying 'working' in console panel

--- Day1/components/Form.js
+++ Day1/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Form = props =>(
-    <form onSubmit={props.getRecipe}>
+    <form onSubmit={props.getRecipe}>  {/* 3. get access to getRecipe in Form.js using props */}
         <input type="text" name="" />
         <button>Search</button>
     </form>
